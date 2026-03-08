2b79a9777aeb2dca047d80a1431cd9c20f32e60c
Alvaromarinho75 <alvarocraftbr@gmail.com>
2026-03-08 12:02:20 -0300

    GET endpoints to list all and find specific order implemented

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -32,3 +32,33 @@ exports.createOrder = async (req, res) => {
         });
     }
 };
+
+// Função para listar TODOS os pedidos
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find(); // Busca tudo no MongoDB
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar a lista de pedidos.", error: error.message });
+    }
+};
+
+// Função para buscar um pedido específico pelo orderId 
+exports.getOrderById = async (req, res) => {
+    try {
+        // Pega o ID que vem na URL (ex: /order/v10089015vdb-01)
+        const orderIdParams = req.params.id; 
+        
+        // Busca no banco onde o campo "orderId" seja igual ao passado na URL
+        const order = await Order.findOne({ orderId: orderIdParams });
+
+        // Se não achar nada, retorna erro 404 (Not Found)
+        if (!order) {
+            return res.status(404).json({ message: "Pedido não encontrado." });
+        }
+
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Erro interno ao buscar o pedido.", error: error.message });
+    }
+};
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,4 +5,10 @@ const orderController = require('../controllers/orderController');
 // Define a rota POST para criar o pedido
 router.post('/', orderController.createOrder);
 
+// Rota para listar todos os pedidos
+router.get('/list', orderController.getAllOrders);
+
+// Rota para buscar um pedido específico
+router.get('/:id', orderController.getOrderById);
+
 module.exports = router;
